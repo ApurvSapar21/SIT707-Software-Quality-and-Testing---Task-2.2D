0005934d9a23927844bbc2fbeee353effa61fd7d
ApurvSapar21 <128551701+ApurvSapar21@users.noreply.github.com>
2024-05-16 23:36:05 +1000

    Update Main.java

--- src/main/java/sit707_week2/Main.java
+++ src/main/java/sit707_week2/Main.java
@@ -1,7 +1,13 @@
 package sit707_week2;
 
-public class Main {
-    public static void main(String[] args) {
-        SeleniumOperations.bunnings_login_page("https://www.bunnings.com.au/login");
+/**
+ * Hello world!
+ * @author Ahsan Habib
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
     }
 }
